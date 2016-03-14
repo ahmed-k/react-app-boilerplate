7c6cd0500aae697fd8c422781cc6e6e15f1a6df1
ahmed <alabdullahwi@saic.com>
2016-03-14 19:47:41 -0400

    first commit to new fork

---
+++ app/KanbanBoard.js
@@ -0,0 +1,35 @@
+/**
+ * Created by alabdullahwi on 2/18/2016.
+ */
+import React, { Component } from 'react'
+import List from './List'
+
+
+class KanbanBoard extends Component {
+    render() {
+
+        return (
+            <div className="app">
+
+                <List id='todo' title="To Do" cards= {
+                  this.props.cards.filter((card) => card.status === "todo")
+                }
+                />
+
+                <List id='in-progress' title="In Progress Cards" cards= {
+                   this.props.cards.filter((card) => card.status === "in-progress")
+                } />
+
+                <List id='done' title="Done Cards" cards= {
+                    this.props.cards.filter((card) => card.status === "done")
+                } />
+
+                </div>
+        );
+
+    }
+
+}
+
+export default KanbanBoard;
+
